c1b99b71c026528d3705dd0df0e39b4a6777a69f
Waber Júnior <waber.junior@madefy.com.br>
2023-11-08 19:10:16 -0300

    Injecting and using service

--- src/app/services/logging.service.ts
+++ src/app/services/logging.service.ts
@@ -4,6 +4,9 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class LoggingService {
-
   constructor() { }
+
+  logStatusChange(status: string) {
+    console.log('A server status changed, new status: ' + status);
+  }
 }
